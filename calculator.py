a88de6bfd25a97a811282a9abc973c9e90c585ac
aieckxis <aleckxiskate@gmail.com>
2023-05-07 19:06:29 +0800

    perform the operation (ZeroDivisionError but error)

--- calculator.py
+++ calculator.py
@@ -13,21 +13,27 @@ while True:
         except ValueError:
             print("Error: Invalid input. Please enter a number.")
     # Calculate the result based on the chosen operation
-    if operation == "addition":
-        result = num1 + num2
-    elif operation == "subtraction":
-        result = num1 - num2
-    elif operation == "multiplication":
-        result = num1 * num2
-    elif operation == "division":
-        if num2 == 0:
-            print("Error: Division by zero is not allowed.")
-            continue
-        else:
-            result = num1 / num2
-    else:
-        print("Error: Invalid operation")
-        continue
+        while True:
+            try:
+                if operation == "addition":
+                    result = num1 + num2
+                elif operation == "subtraction":
+                    result = num1 - num2
+                elif operation == "multiplication":
+                    result = num1 * num2
+                elif operation == "division":
+                    if num2 == 0:
+                        raise ZeroDivisionError("Error: Cannot divide by zero.")
+                    result = num1 / num2
+                else:
+                    raise ValueError("Error: Invalid operation")
+                    break
+            except ZeroDivisionError as e:
+                print(f"Error: {str(e)} Please enter the second number again: ")
+                num2 = float(input("Enter the second number: "))
+            except ValueError as e:
+                print(f"Error: {str(e)}")
+                break
     # Print the result
     print("Result: ", result)
 # Ask the user if they want to try again
