8d84635979409241b8346fd9456e7cea79510b64
aieckxis <aleckxiskate@gmail.com>
2023-05-07 17:26:37 +0800

    ask the user to enter two numbers

--- calculator.py
+++ calculator.py
@@ -2,8 +2,16 @@
 # BSCpE 1-4
 
 # Ask the user for the operation to perform
-operation = input("Please choose a mathe operation (addition, subtraction, multiplication, division): ")
+while True:
+    operation = input("Please choose a math operation (addition, subtraction, multiplication, division): ")
 # Ask the user for two numbers
+    while True:
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+            break
+        except ValueError:
+            print("Error: Invalid input. Please enter a number.")
 # Calculate the result based on the chosen operation
 # Print the result
 print(operation)
