63475443e4e28899a1995c1a95ece02133f05a76
aieckxis <aleckxiskate@gmail.com>
2023-05-07 17:21:19 +0800

    ask the user to choose operation

--- calculator.py
+++ calculator.py
@@ -2,7 +2,9 @@
 # BSCpE 1-4
 
 # Ask the user for the operation to perform
+operation = input("Please choose a mathe operation (addition, subtraction, multiplication, division): ")
 # Ask the user for two numbers
 # Calculate the result based on the chosen operation
 # Print the result
+print(operation)
 # Ask the user if they want to try again
