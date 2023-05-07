1fd516b4588691b8d21fb51822cfb8db865350d2
aieckxis <aleckxiskate@gmail.com>
2023-05-07 17:18:34 +0800

    pseudocode

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# Bernabe, Aleckxis Kate V.
+# BSCpE 1-4
+
+# Ask the user for the operation to perform
+# Ask the user for two numbers
+# Calculate the result based on the chosen operation
+# Print the result
+# Ask the user if they want to try again
